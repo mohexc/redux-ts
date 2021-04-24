bb0759998d117d480603f0af13b5651f3c7bc688
nanut <galunyu@gmail.com>
2021-04-24 08:30:04 +0700

    [Start] redux-template

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { Dispatch } from 'react'
+import { ActionType } from '../action-types'
+import { Actions } from '../actions'
+
+export const searchRepositories = (term: string) => {
+    return async (dispatch: Dispatch<Actions>) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES
+        })
+
+        try {
+
+            const { data } = await axios.get('https://registry.npmjs.org/-/v1/search?text=react', {
+                params: {
+                    text: term
+                }
+            })
+
+            const names = data.object.map((result: any) => {
+                return result.package.name
+            })
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            })
+
+            return
+
+        } catch (err) {
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                payload: err.message
+            })
+        }
+    }
+}
